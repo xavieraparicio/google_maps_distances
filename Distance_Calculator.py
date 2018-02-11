74389f2fff1caf14e1868fd29706336cb2dddd95
xavieraparicio <xavier.aparicioangles.ac@gmail.com>
2018-02-11 18:24:20 +0000

    added .gitignore and Distance_Calculator.py

---
+++ Distance_Calculator.py
@@ -0,0 +1,10 @@
+import urllib.request
+import json
+import pandas as pd
+import numpy as np
+from operator import itemgetter
+
+import api_key_file
+
+class Distance_Calculator(api_key):
+    def __init__
