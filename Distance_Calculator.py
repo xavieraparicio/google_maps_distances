c788cc67014d04811ae095b9b64c26cae0690a82
xavieraparicio <xavier.aparicioangles.ac@gmail.com>
2018-02-11 19:56:02 +0000

    create a string for future api request

--- Distance_Calculator.py
+++ Distance_Calculator.py
@@ -4,18 +4,43 @@ import pandas as pd
 import numpy as np
 from operator import itemgetter
 
-import api_key_file
+#import api_key_file
 
 class Distance_Calculator:
     def hello_world_method(self):
         print("hello world")
 
-    def reading_postcodes_for_origin_point(self,csv_file_with_postcodes_for_origin_point):
-        postcodes_for_origin_point = pd.read_csv(csv_file_with_postcodes_for_origin_point)
-        return(postcodes_for_origin_point)
+    def read_postcodes(self,postcodes_csv):
+        df_postcodes = pd.read_csv(postcodes_csv)
+        return(df_postcodes)
 
-    def printing_read_postcodes(self,csv_file_with_postcodes_for_origin_point):
-        postcodes_for_origin_point = self.reading_postcodes_for_origin_point(csv_file_with_postcodes_for_origin_point)
-        for postcode in postcodes_for_origin_point.Postcodes:
+    def print_postcodes(self,df_Postcodes):
+        #df_Postcodes = self.read_postcodes(postcodes_csv)
+        for postcode in df_Postcodes.Postcodes:
             print(postcode)
         return
+
+    def create_string_for_request(self,df_Postcodes):
+        string = ''
+        for index, postcode in enumerate(df_Postcodes.Postcodes):
+            string += postcode.replace(" ","+")
+            if (index+1) != df_Postcodes.shape[0]:
+                string += "|"
+        return(string)
+
+
+
+
+
+
+
+# def create_final_destination_string(final_postcode_dataframe):
+#
+#     destination = ''
+#     for idx, postcode in enumerate(final_postcode_dataframe.Postcode):
+#         if (idx + 1) != final_postcode_dataframe.shape[0]:
+#             destination += postcode.replace(" ","+") + "House+of+Fraser" + '|'
+#         else :
+#             destination += postcode.replace(" ","+") + 'House+of+Fraser'
+#
+#     return(destination)
