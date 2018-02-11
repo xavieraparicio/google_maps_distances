5d713cea4056aa439039c90c6c8753bd75a01d2c
xavieraparicio <xavier.aparicioangles.ac@gmail.com>
2018-02-11 19:17:40 +0000

    Read and Print origin point postcodes

--- Distance_Calculator.py
+++ Distance_Calculator.py
@@ -7,5 +7,15 @@ from operator import itemgetter
 import api_key_file
 
 class Distance_Calculator:
-    def empty_method(self):
-        print("hello world! yaaas")
+    def hello_world_method(self):
+        print("hello world")
+
+    def reading_postcodes_for_origin_point(self,csv_file_with_postcodes_for_origin_point):
+        postcodes_for_origin_point = pd.read_csv(csv_file_with_postcodes_for_origin_point)
+        return(postcodes_for_origin_point)
+
+    def printing_read_postcodes(self,csv_file_with_postcodes_for_origin_point):
+        postcodes_for_origin_point = self.reading_postcodes_for_origin_point(csv_file_with_postcodes_for_origin_point)
+        for postcode in postcodes_for_origin_point.Postcodes:
+            print(postcode)
+        return
